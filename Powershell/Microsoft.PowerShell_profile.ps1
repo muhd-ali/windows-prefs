2c3ee4bc586cde818a8a25929848701c710ffc4b
Muhammad Ali <this.is.muhammad.ali@gmail.com>
2020-09-25 22:18:52 -0500

    Added prefs

---
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,11 @@
+try { $null = gcm pshazz -ea stop; pshazz init 'default' } catch { }
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Paradox
